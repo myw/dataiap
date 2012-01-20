fa3179f31f984241fea716854e53513c3b000451
Misha Wolfson <mywolfson@gmail.com>
2012-01-19 23:43:34 -0500

    Cleaned up boto version of TF-IDF code

--- day5/work/mr_tfidf_per_sender_aws.py
+++ day5/work/mr_tfidf_per_sender_aws.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python
 
 import sys
-sys.path.append('..')
+from cStringIO import StringIO
 
 from mrjob.protocol import JSONValueProtocol
 from mrjob.job import MRJob
-from term_tools import get_terms
-
-AWS_DIRECTORY = "dataiap-wolfsonm-student55-testbucket/idf_parts"
-
-awskey = "AKIAJEMFAHAMY7PUJ34Q"
-awssecret = "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"
+from mrjob.emr import parse_s3_uri
 
 from boto.s3.connection import S3Connection
+from term_tools import get_terms
 
 class MRTFIDFBySender(MRJob):
     INPUT_PROTOCOL = JSONValueProtocol
     OUTPUT_PROTOCOL = JSONValueProtocol
+
+    def configure_options(self):
+        super(MRTFIDFBySender, self).configure_options()
+        self.add_passthrough_option(
+            '--idf_loc', type='str', default='',
+            help='The s3:// URL for the location of the IDFs')
+        self.add_passthrough_option(
+            '--aws_access_key_id', type='str', default='',
+            help='The access key ID for the AWS account')
+        self.add_passthrough_option(
+            '--aws_secret_access_key', type='str', default='',
+            help='The secret access key for the AWS account')
 
     def mapper(self, key, email):
         for term in get_terms(email['text']):
@@ -24,30 +32,23 @@ class MRTFIDFBySender(MRJob):
 
     def reducer_init(self):
         self.idfs = {}
-        conn = S3Connection(awskey, awssecret)
-        bucket_name, search_string = AWS_DIRECTORY.split('/')
-        bucket = conn.get_bucket(bucket_name)
 
-        for key in bucket.list(search_string):
+        conn = S3Connection(self.options.aws_access_key_id,
+                            self.options.aws_secret_access_key)
 
-            buffer = ""
+        # Iterate through the files in the bucket provided by the user
+        bucket, folder = parse_s3_uri("s3://" + self.options.idf_loc)
+        for key in conn.get_bucket(bucket).list(folder):
 
-            for bytes in key: # iterate through the key, buffering into lines yourself
-
-                buffer += bytes # add to the buffer
-
-                # If our buffer includes any complete lines
-                if "\n" in buffer:
-                    lines = buffer.split('\n')
-                    buffer = lines.pop()
-
-                    for line in lines:
-                        term_idf = JSONValueProtocol.read(line)[1] # parse the line as a JSON object
-                        self.idfs[term_idf['term']] = term_idf['idf']
+            # Load the whole file first, then read it line-by-line: otherwise,
+            # chunks may not be even lines
+            for line in StringIO(key.get_contents_as_string()): 
+                term_idf = JSONValueProtocol.read(line)[1] # parse the line as a JSON object
+                self.idfs[term_idf['term']] = term_idf['idf']
 
     def reducer(self, term_sender, howmany):
         tfidf = sum(howmany) * self.idfs[term_sender['term']]
-        yield None, {'term_sender': term_sender, 'tfidf': tfidf}
+        yield None, {'sender': term_sender['sender'], 'term': term_sender['term'], 'tfidf': tfidf}
 
 if __name__ == '__main__':
         MRTFIDFBySender.run()
